c56705cc643792c4a85a43e76db6069664b0ac7d
PerlaRuano <perla11916@cccneb.edu>
2024-11-21 23:53:48 -0600

    Update file changes

--- my-project/src/App.vue
+++ my-project/src/App.vue
@@ -1,5 +1,13 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
+  <div class="profile">
+    <header>
+      <h1>Perla Ruano</h1>
+  <img alt="profile" src="./assets/Perla.png">
+    </header>
+    <main>
+      <router-view></router-view> <!-- This will render the current route's component -->
+    </main>
+  </div>
   <HelloWorld msg="Welcome to Your Vue.js App"/>
 </template>
 
@@ -22,5 +30,12 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+.profile img{
+  height: 250px;
+}
+
+.template{
+  background-color: aquamarine;
 }
 </style>
